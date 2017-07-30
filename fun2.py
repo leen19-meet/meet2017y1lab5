1e4bdc6497e4e4f31b79e8084645b95f674b181e
leen19-meet <leen19@meet.mit.edu>
2017-07-30 22:52:27 +0300

    Stopping here

--- fun2.py
+++ fun2.py
@@ -1,8 +1,23 @@
 
 def draw_1d(n, char):
     print(char*n)
-c1= "*"
-draw_1d(10, c1)
+##
+##
+##c1= "*"
+##c2= "*"
+##c3= "*"
+##c4= "*"
+##c5= "*"
+##draw_1d(10, c1)
+##draw_1d(10, c2)
+##draw_1d(10, c3)
+##draw_1d(10, c4)
+##draw_1d(10, c5)
 
 
-
+def draw_2d(n, m ,char):
+    # n - numbers of characters in 1 row
+    # m - number of rows
+    # char - character to be repeated
+    for number in range(m):
+        print(n*char)
