fba5842ddecd4aab14aa7be93e045253e89e8ecf
leen19-meet <leen19@meet.mit.edu>
2017-07-30 13:59:09 +0300

    Stopping here

--- fun1.py
+++ fun1.py
@@ -1,10 +1,18 @@
+#making functions
 
-
-def add_numbers():
+def add_numbers(start,end):
     c=0
-    for number in range(1, 100 + 1):
+    for number in range(start, end + 1):
         print(number)
         c=c + number
     return c
-answer= add_numbers()
+
+
+
+
+
+#########################################################
+#Using function
+
+answer= add_numbers(333, 777)
 print(answer)
---
+++ fun2.py
@@ -0,0 +1,8 @@
+
+def draw_1d(n, char):
+    print(char*n)
+c1= "*"
+draw_1d(10, c1)
+
+
+
